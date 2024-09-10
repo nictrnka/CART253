91f8eddcd66b3ddf3100157dd229c3524c66863b
2nic <140764920+2nic@users.noreply.github.com>
2024-09-10 15:24:01 -0400

    A Vinyl Record

--- topics/drawing-practice/js/script.js
+++ topics/drawing-practice/js/script.js
@@ -1,18 +1,18 @@
 /**
- * Title of Project
- * Author Name
+ * Drawing Practice
+ * Nic Trnka
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * im a good drawer
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * 
 */
 function setup() {
-
+    //A nice square canvas to work with
+createCanvas(640,640);
 }
 
 
@@ -20,5 +20,35 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    // a grey background
+    background(150,150,150);
+    // A red record
+
+    //first we PUSH to remember the previous settings
+    push();
+    //then we change the settings for this shape
+    fill(255,0,0);
+    stroke(255,255,255);
+    //then we draw the shape
+    ellipse(320,320,480,480);
+    //finally we POP to restore the original settings
+    pop();
+
+    //the label on the record
+    push();
+    fill(255,255,255);
+    noStroke();
+    ellipse(320,320,140,140);
+    pop();
+
+    //the hole in the record
+    push();
+    fill(150,150,150);
+    stroke(50,50,50);
+    ellipse(320,320,20,20);
+    pop();
+
+
+
 
 }

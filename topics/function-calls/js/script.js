5a2abc8b3fd2dd6b2f74b38ef68491026def78b3
2nic <140764920+2nic@users.noreply.github.com>
2024-09-10 14:14:01 -0400

    Function Calls

---
+++ topics/function-calls/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Function Calls
+ * Nic Trnka
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+createCanvas(640, 480);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+background(255, 100, 100);
+rect(200, 80, 240, 320);
+}

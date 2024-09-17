683516d27c28c912624aa23ed3bec2327783a0fe
2nic <140764920+2nic@users.noreply.github.com>
2024-09-16 21:20:02 -0400

    made tha bird move

--- topics/movement-variables/js/script.js
+++ topics/movement-variables/js/script.js
@@ -1,24 +1,53 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Nic Trnka
+ * movement variables
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50,
+    velocity: {
+        x: 1,
+        y: -2
+    },
+    minVelocity: {
+        x: -3,
+        y: -2
+    },
+    maxVelocity: {
+        x: 3,
+        y: 2
+    },
+    acceleration: {
+        x: 0.025,
+        // - so bird goes UP
+        y: -0.05
+    }
 
 }
 
+function setup() {
+    createCanvas(640, 480);
+}
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(0);
 
+    //change velocity by adding acceleration to it
+    bird.velocity.x += bird.acceleration.x;
+    bird.velocity.y += bird.acceleration.y;
+
+    //constrain the velocity
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    //move the bird
+    bird.x += bird.velocity.x;
+    bird.y += bird.velocity.y;
+
+    //draw the bird
+    ellipse(bird.x, bird.y, bird.size);
 }

1288806d67987b05ef006bca54d962abec8e3e3a
2nic <140764920+2nic@users.noreply.github.com>
2024-10-01 00:02:43 -0400

    finished plain javascript events

--- topics/plain-javascript-events/js/script.js
+++ topics/plain-javascript-events/js/script.js
@@ -1,24 +1,41 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
-
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
-
+// The background colour
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "#000000",
+        white: "#ffffff"
+    },
+    switchKey: 32 // Space bar
 }
 
+/**
+ * Create the canvas and set up event listener
+ */
+function setup() {
+    createCanvas(400, 400);
+
+    // Listen for key down
+    window.addEventListener("keydown", changeBG);
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Fill the canvas
+ */
 function draw() {
+    background(bg.fill);
+}
 
+/**
+ * Switch the background
+ */
+function changeBG(event) { // NOTE the event parameter
+    // Use event.keyCode to check if they pressed the switching key...
+    if (event.keyCode === bg.switchKey) {
+        if (bg.fill === bg.fills.black) {
+            bg.fill = bg.fills.white;
+        }
+        else {
+            bg.fill = bg.fills.black;
+        }
+    }
 }

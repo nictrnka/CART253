91b23869250368f17cf07d08b14aec5357322f6a
2nic <140764920+2nic@users.noreply.github.com>
2024-10-22 17:21:36 -0400

    Made frog tongue move in the direction of the mouse
    Used math.atan2, math.cos, and math.sin to calculate direction from frog body to mouse, and move frog tongue in that direction. Made the frog tongue stop moving at all borders

--- topics/mod-jam/js/script.js
+++ topics/mod-jam/js/script.js
@@ -19,7 +19,7 @@
 const frog = {
     // The frog's body has a position and size
     body: {
-        x: 320,
+        x: 350,
         y: 520,
         size: 150
     },
@@ -27,10 +27,20 @@ const frog = {
     tongue: {
         x: undefined,
         y: 480,
+
         size: 20,
         speed: 20,
         // Determines how the tongue moves each frame
-        state: "idle" // State can be: idle, outbound, inbound
+        state: "idle", // State can be: idle, outbound, inbound
+        direction: {
+            x: undefined,
+            y: undefined,
+
+        },
+        velocity: {
+            x: undefined,
+            y: undefined,
+        }
     }
 };
 
@@ -47,7 +57,7 @@ const fly = {
  * Creates the canvas and initializes the fly
  */
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(700, 700);
 
     // Give the fly its first random position
     resetFly();
@@ -99,30 +109,50 @@ function resetFly() {
  * Moves the frog to the mouse position on x
  */
 function moveFrog() {
-    frog.body.x = mouseX;
+    //frog.body.x = mouseX;
 }
 
 /**
  * Handles moving the tongue based on its state
  */
 function moveTongue() {
+
     // Tongue matches the frog's x
-    frog.tongue.x = frog.body.x;
+
     // If the tongue is idle, it doesn't do anything
     if (frog.tongue.state === "idle") {
         // Do nothing
+        frog.tongue.x = frog.body.x;
+        frog.tongue.y = frog.body.y;
     }
     // If the tongue is outbound, it moves up
     else if (frog.tongue.state === "outbound") {
-        frog.tongue.y += -frog.tongue.speed;
-        // The tongue bounces back if it hits the top
+
+        frog.tongue.y += frog.tongue.velocity.y * frog.tongue.speed;
+        frog.tongue.x += frog.tongue.velocity.x * frog.tongue.speed;
+        console.log(frog.tongue.x);
+
+        // The tongue bounces back if it hits the borders
         if (frog.tongue.y <= 0) {
             frog.tongue.state = "inbound";
         }
+        if (frog.tongue.y >= height) {
+            frog.tongue.state = "inbound";
+        }
+        if (frog.tongue.x <= 0) {
+            frog.tongue.state = "inbound";
+        }
+        if (frog.tongue.x >= width) {
+            frog.tongue.state = "inbound";
+        }
+
+
     }
     // If the tongue is inbound, it moves down
     else if (frog.tongue.state === "inbound") {
-        frog.tongue.y += frog.tongue.speed;
+        frog.tongue.y -= frog.tongue.velocity.y * frog.tongue.speed;
+        frog.tongue.x -= frog.tongue.velocity.x * frog.tongue.speed;
+
         // The tongue stops if it hits the bottom
         if (frog.tongue.y >= height) {
             frog.tongue.state = "idle";
@@ -134,6 +164,14 @@ function moveTongue() {
  * Displays the tongue (tip and line connection) and the frog (body)
  */
 function drawFrog() {
+
+    // Draw the frog's body
+    push();
+    fill("#00ff00");
+    noStroke();
+    ellipse(frog.body.x, frog.body.y, frog.body.size);
+    pop();
+
     // Draw the tongue tip
     push();
     fill("#ff0000");
@@ -148,12 +186,6 @@ function drawFrog() {
     line(frog.tongue.x, frog.tongue.y, frog.body.x, frog.body.y);
     pop();
 
-    // Draw the frog's body
-    push();
-    fill("#00ff00");
-    noStroke();
-    ellipse(frog.body.x, frog.body.y, frog.body.size);
-    pop();
 }
 
 /**
@@ -163,7 +195,7 @@ function checkTongueFlyOverlap() {
     // Get distance from tongue to fly
     const d = dist(frog.tongue.x, frog.tongue.y, fly.x, fly.y);
     // Check if it's an overlap
-    const eaten = (d < frog.tongue.size/2 + fly.size/2);
+    const eaten = (d < frog.tongue.size / 2 + fly.size / 2);
     if (eaten) {
         // Reset the fly
         resetFly();
@@ -176,7 +208,18 @@ function checkTongueFlyOverlap() {
  * Launch the tongue on click (if it's not launched yet)
  */
 function mousePressed() {
+
     if (frog.tongue.state === "idle") {
+
+        frog.tongue.direction.x = mouseX - frog.body.x;
+        frog.tongue.direction.y = mouseY - frog.body.y;
+
+        var angle = Math.atan2(frog.tongue.direction.y, frog.tongue.direction.x);
+
+        var magnitude = 1.0;
+        frog.tongue.velocity.x = Math.cos(angle) * magnitude;
+        frog.tongue.velocity.y = Math.sin(angle) * magnitude;
+
         frog.tongue.state = "outbound";
     }
 }

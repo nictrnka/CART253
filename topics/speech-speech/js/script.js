17c44d522a73c01a03a9921551c60f0e2e9bfc4a
2nic <140764920+2nic@users.noreply.github.com>
2024-11-19 15:28:11 -0500

    completed sequential arrays in practice

--- topics/speech-speech/js/script.js
+++ topics/speech-speech/js/script.js
@@ -8,11 +8,15 @@
 
 "use strict";
 
+let speech = ["Veni.", "Vidi.", "Vici.", "Sensi malum."];
+
+let speechIndex = 0;
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(600, 100);
 }
 
 
@@ -20,5 +24,25 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    //get the current line of our speech
+    let currentLine = speech[speechIndex];
+
+    //display it
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentLine, width / 2, height / 2);
+    pop();
+
+}
+
+function mousePressed() {
+    speechIndex = speechIndex + 1;
+
+    if (speechIndex >= speech.length) {
+        speechIndex = 0;
+    }
 }

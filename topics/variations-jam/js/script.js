d29b152c175c8213f85aa7634a551c38cf27f813
2nic <140764920+2nic@users.noreply.github.com>
2025-01-22 18:58:29 -0500

    working paddle movement
    Started work on a "brick breaker" type game, added a ball & paddle & made the paddle move with A & D. paddle stays still if both keys are pressed or none are pressed, and moves left or right if only one key is pressed.

--- topics/variations-jam/js/script.js
+++ topics/variations-jam/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Title of Project
- * Author Name
+ * Nic Trnka
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -8,17 +8,97 @@
 
 "use strict";
 
+let paddle = {
+    width: 50,
+    height: 10,
+    x: 300,
+    y: 550,
+
+    r: 100,
+    g: 100,
+    b: 100,
+
+    velocity: 0,
+    speed: 10,
+
+}
+
+let ball = {
+    width: 10,
+    height: 10,
+    x: 300,
+    y: 300,
+
+    r: 200,
+    g: 100,
+    b: 100,
+
+    velocity: 0,
+    speed: {
+        x: 10,
+        y: 10,
+    }
+}
+
+
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(600, 600);
+    background(0);
 }
-
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+    drawBackground();
+    movePaddle();
+    drawPaddle();
+    drawBall();
 }
+
+function drawBackground() {
+    background(0);
+}
+
+function movePaddle() {
+
+    if (keyIsDown(65) && keyIsDown(68)) {
+        paddle.velocity = 0;
+    }
+    else if (keyIsDown(65)) {
+        paddle.velocity = -1;
+    }
+    else if (keyIsDown(68)) {
+        paddle.velocity = 1;
+    }
+    else {
+        paddle.velocity = 0;
+    }
+
+    paddle.x += paddle.velocity * paddle.speed;
+}
+
+function drawPaddle() {
+    push();
+    fill(paddle.r, paddle.g, paddle.b);
+    rectMode(CENTER);
+    rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    pop();
+}
+
+function drawBall() {
+    push();
+    fill(ball.r, ball.g, ball.b);
+    rectMode(CENTER);
+    rect(ball.x, ball.y, ball.width, ball.height);
+}
+
+
+
+
+
+

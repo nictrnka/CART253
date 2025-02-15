41d36a2d39f8c30ecd2a9b0ca4f19d14286712da
2nic <140764920+2nic@users.noreply.github.com>
2025-02-15 01:49:31 -0500

    added colours + different notes for each key
    each key has its own palette and the synth selects notes from separate key arrays

--- topics/variations-jam/js/script.js
+++ topics/variations-jam/js/script.js
@@ -12,7 +12,14 @@ let screen = "menu"
 let polySynth;
 let synth = {
     lastNote: 'C4',
-    key: "c major"
+    key: "one"
+}
+let keys = {
+
+    one: ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'],
+    two: ['C#4', 'D#4', 'E4', 'F#4', 'G#4', 'A4', 'B4', 'C#5'],
+    three: ['C4', 'C#4', 'D#4', "F4", 'G4', 'G#4', "A#4", "C5"]
+
 }
 
 let menuButtons = {
@@ -92,10 +99,6 @@ let bricks = [
         height: 20,
         x: undefined,
         y: undefined,
-
-        r: 0,
-        g: 0,
-        b: 0,
     },
 
 ]
@@ -115,6 +118,58 @@ let brickColors = {
         r: 89,
         g: 91,
         b: 124,
+    },
+
+    keyOne: {
+        three: {
+            r: 183,
+            g: 188,
+            b: 162,
+        },
+        two: {
+            r: 121,
+            g: 135,
+            b: 102,
+        },
+        one: {
+            r: 69,
+            g: 89,
+            b: 81,
+        },
+    },
+    keyTwo: {
+        three: {
+            r: 201,
+            g: 165,
+            b: 169,
+        },
+        two: {
+            r: 149,
+            g: 129,
+            b: 157,
+        },
+        one: {
+            r: 89,
+            g: 91,
+            b: 124,
+        },
+    },
+    keyThree: {
+        three: {
+            r: 235,
+            g: 199,
+            b: 144,
+        },
+        two: {
+            r: 183,
+            g: 99,
+            b: 91,
+        },
+        one: {
+            r: 112,
+            g: 79,
+            b: 79,
+        },
     }
 
 }
@@ -178,8 +233,8 @@ function drawMenu() {
     textAlign(CENTER);
     text('♫ brick baroque ♫', width / 2, 200);
     fill(ball.r, ball.g, ball.b);
-    text('c major', menuButtons.keyOne.x, menuButtons.keyOne.y);
-    text('c minor', menuButtons.keyTwo.x, menuButtons.keyTwo.y);
+    text('c aeolian', menuButtons.keyOne.x, menuButtons.keyOne.y);
+    text('c# minor', menuButtons.keyTwo.x, menuButtons.keyTwo.y);
     text('f minor', menuButtons.keyThree.x, menuButtons.keyThree.y);
     pop();
 }
@@ -190,21 +245,21 @@ function mousePressed() {
             mouseX < menuButtons.keyOne.x + menuButtons.width / 2 &&
             mouseY > -5 + menuButtons.keyOne.y - menuButtons.height / 2 &&
             mouseY < -5 + menuButtons.keyOne.y + menuButtons.height / 2) {
-            key = "one";
+            synth.key = "one";
             startGame();
         }
         else if (mouseX > menuButtons.keyTwo.x - menuButtons.width / 2 &&
             mouseX < menuButtons.keyTwo.x + menuButtons.width / 2 &&
             mouseY > -5 + menuButtons.keyTwo.y - menuButtons.height / 2 &&
             mouseY < -5 + menuButtons.keyTwo.y + menuButtons.height / 2) {
-            key = "two";
+            synth.key = "two";
             startGame();
         }
         else if (mouseX > menuButtons.keyThree.x - menuButtons.width / 2 &&
             mouseX < menuButtons.keyThree.x + menuButtons.width / 2 &&
             mouseY > -5 + menuButtons.keyThree.y - menuButtons.height / 2 &&
             mouseY < -5 + menuButtons.keyThree.y + menuButtons.height / 2) {
-            key = "three";
+            synth.key = "three";
             startGame();
         }
     }
@@ -212,6 +267,47 @@ function mousePressed() {
 
 
 function startGame() {
+
+    if (synth.key === "one") {
+        brickColors.three.r = brickColors.keyOne.three.r;
+        brickColors.three.g = brickColors.keyOne.three.g;
+        brickColors.three.b = brickColors.keyOne.three.b;
+
+        brickColors.two.r = brickColors.keyOne.two.r;
+        brickColors.two.g = brickColors.keyOne.two.g;
+        brickColors.two.b = brickColors.keyOne.two.b;
+
+        brickColors.one.r = brickColors.keyOne.one.r;
+        brickColors.one.g = brickColors.keyOne.one.g;
+        brickColors.one.b = brickColors.keyOne.one.b;
+    }
+    else if (synth.key === "two") {
+        brickColors.three.r = brickColors.keyTwo.three.r;
+        brickColors.three.g = brickColors.keyTwo.three.g;
+        brickColors.three.b = brickColors.keyTwo.three.b;
+
+        brickColors.two.r = brickColors.keyTwo.two.r;
+        brickColors.two.g = brickColors.keyTwo.two.g;
+        brickColors.two.b = brickColors.keyTwo.two.b;
+
+        brickColors.one.r = brickColors.keyTwo.one.r;
+        brickColors.one.g = brickColors.keyTwo.one.g;
+        brickColors.one.b = brickColors.keyTwo.one.b;
+    }
+    else if (synth.key === "three") {
+        brickColors.three.r = brickColors.keyThree.three.r;
+        brickColors.three.g = brickColors.keyThree.three.g;
+        brickColors.three.b = brickColors.keyThree.three.b;
+
+        brickColors.two.r = brickColors.keyThree.two.r;
+        brickColors.two.g = brickColors.keyThree.two.g;
+        brickColors.two.b = brickColors.keyThree.two.b;
+
+        brickColors.one.r = brickColors.keyThree.one.r;
+        brickColors.one.g = brickColors.keyThree.one.g;
+        brickColors.one.b = brickColors.keyThree.one.b;
+    }
+
     screen = "game";
     spawnGrid();
     spawnBall();
@@ -258,15 +354,26 @@ function spawnGrid() {
 
 function playSynth(paddleHit) {
     userStartAudio();
-
-    let note = random(['A4', 'B4', 'C#4', 'D#4', 'E4', 'F#4', 'G#4']);
-    while (note === synth.lastNote) {
-        note = random(['A4', 'B4', 'C#4', 'D#4', 'E4', 'F#4', 'G#4']);
+    let note;
+    if (synth.key === "one") {
+        note = random(keys.one);
+        while (note === synth.lastNote) {
+            note = random(keys.one);
+        }
+    }
+    else if (synth.key === 'two') {
+        note = random(keys.two);
+        while (note === synth.lastNote) {
+            note = random(keys.two);
+        }
+    }
+    else if (synth.key === 'three') {
+        note = random(keys.three);
+        while (note === synth.lastNote) {
+            note = random(keys.three);
+        }
     }
 
-    if (paddleHit === true) {
-
-    }
     // note velocity (volume, from 0 to 1)
     let velocity = 1;
     // time from now (in seconds)
@@ -274,7 +381,7 @@ function playSynth(paddleHit) {
     // note duration (in seconds)
     let dur = 1 / 20;
 
-    polySynth.setADSR(0.3, [1], [1], [1]);
+    polySynth.setADSR(1, [1], [1], [1]);
     polySynth.play(note, velocity, time, dur);
     synth.lastNote = note;
 }
@@ -320,8 +427,8 @@ function createBrick() {
 
 function keyPressed() {
 
-    if (key === 'r' && ball.alive === false) {
-        spawnBall();
+    if (keyCode === 27 && screen === 'game') {
+        screen = 'menu';
     }
 }
 
